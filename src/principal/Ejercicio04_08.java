26bedfaaf2cf969bbcbb5c8ad5702e842d23cad5
Y <Y@DESKTOP-KJUN4CO>
2023-06-26 12:33:14 +0200

    EJERCICIO 8 Y 11

---
+++ src/principal/Ejercicio04_08.java
@@ -0,0 +1,10 @@
+package principal;
+
+public class Ejercicio04_08 {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+
+	}
+
+}
---
+++ src/principal/Ejercicio04_11.java
@@ -0,0 +1,10 @@
+package principal;
+
+public class Ejercicio04_11 {
+
+	public static void main(String[] args) {
+		// Dados dos vectores, calcular la suma de ambos. 
+
+	}
+
+}
